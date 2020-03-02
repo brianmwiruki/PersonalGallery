44b716c5a88634e373d84d6913efcacb90dcba20
Brian Mwiruki <mwiruki@outlook.com>
2020-03-02 21:31:58 +0300

    pictures method in views

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+from .models import Picture
+import datetime as dt
 
 # Create your views here.
+
+def picture(request,picture_id):
+
+    try: 
+        picture = Picture.objects.get(id = picture_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request, "pictures.html", {"picture": picture})
