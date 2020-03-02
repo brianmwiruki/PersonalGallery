23c8647d357c5a490ff08441bbdb98f94d59cb5c
Brian Mwiruki <mwiruki@outlook.com>
2020-03-02 21:00:07 +0300

    pictures class initialization

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Picture(models.Model):
+    title = models.CharField(max_length = 60)
+    post = models.TextField()
+    editor = models.ForeignKey(Editor, on_delete = models.CASCADE)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add = True)
+    article_image = models.ImageField(upload_to = 'articles/',default = 'image' )
