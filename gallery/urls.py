b5dba00a84217756151379bba94fae0f0141bc2e
Brian Mwiruki <mwiruki@outlook.com>
2020-03-03 09:26:26 +0300

    static file initialization in settings

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,3 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^picture/(\d+)', views.picture,name = 'picture'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- personalGallery/settings.py
+++ personalGallery/settings.py
@@ -19,6 +19,18 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '$#e8gvy+sv0bv81sj!a5tx%^%lqm665638qal6otj(d4(zj0p('
 
