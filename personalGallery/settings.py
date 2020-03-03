d6682e7f17536ca0225d9f4eefd8bc20a779dece
Brian Mwiruki <mwiruki@outlook.com>
2020-03-03 13:55:01 +0300

    stylesheet link add in base html

--- personalGallery/settings.py
+++ personalGallery/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'personalGallery.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'pictures',
+        'NAME': 'gallery',
         'USER': 'bryan',
         'PASSWORD': 'Admin',
     }
