855678eae6163d20fbbb32a6c797cf02a00ea65e
Brian Mwiruki <mwiruki@outlook.com>
2020-03-03 09:37:32 +0300

    initialization of the picture class in apps in galleyadmin class

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Picture
 
 # Register your models here.
+
+class GalleryAdmin(admin.ModelAdmin):
+    
+    admin.site.register(Picture)
--- personalGallery/settings.py
+++ personalGallery/settings.py
@@ -43,6 +43,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery.apps.galleryConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
