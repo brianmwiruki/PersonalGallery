35dd76fbd5ce4ebda388740cec934b7d1b0d9e70
Brian Mwiruki <mwiruki@outlook.com>
2020-03-03 12:46:43 +0300

    initialization of category and search regex in the gallery/url file for routing

--- gallery/urls.py
+++ gallery/urls.py
@@ -7,4 +7,18 @@ urlpatterns = [
     url(r'^picture/(\d+)', views.picture,name = 'picture')
 ]
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, dfrom django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    url(r'^$',views.art,name='art'),
+    url(r'^categories/(\w+)', views.get_category,name='get_category'),
+    url(r'^location/(\w+)', views.get_location,name='get_location'),
+    url(r'^search/',views.search_results, name='search_results'),
+
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
