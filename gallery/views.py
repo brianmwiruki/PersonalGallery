d3df03b7eb4f0a5a126f1b74a6c9c4c206f7b170
Brian Mwiruki <mwiruki@outlook.com>
2020-03-03 12:42:47 +0300

    views update to include search results and get category methods

--- gallery/views.py
+++ gallery/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse,Http404
-from .models import Picture
-import datetime as dt
+from django.shortcuts import render
+from .models import Image,Category,Location
+
 
 # Create your views here.
+def art(request):
+    art_images = Image.objects.all()
+    categories = Category.objects.all()
+    location_results = Location.objects.all()
 
-def picture(request,picture_id):
+    return render(request,'index.html',{'art_images':art_images,'categories':categories,'location_results':location_results})
 
-    try: 
-        picture = Picture.objects.get(id = picture_id)
-    except DoesNotExist:
-        raise Http404()
-    return render(request, "pictures.html", {"picture": picture})
+def search_results(request):
+    
+    if 'searchItem' in request.GET and request.GET["searchItem"]:
+        search_term = request.GET.get("searchItem")
+        searched_image = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'index.html',{"message":message,"art_images": searched_image})
+
+    else:
+        message = "No Image for the searched term. Search again."
+        return render(request, 'index.html',{"message":message})
+
+def get_category(request,category):
+    categories = Category.objects.all()
+    location_results = Location.objects.all()
+    category_result = Image.objects.filter(image_category__category_name = category)
+
+    return render(request,'index.html',{'art_images':category_result,'categories':categories,'location_results':location_results})
+
+def get_location(request,location):
+    categories = Category.objects.all()
+    location_results = Location.objects.all()
+    location_result = Image.objects.filter(image_location__location_name= location)
+
+    return render(request,'index.html',{'art_images':location_result,'categories':categories,'location_results':location_results})
+
